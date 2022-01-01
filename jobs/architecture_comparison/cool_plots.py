33aa94189a42225c07cc658e0c3bb283f4a36d7b
yvictorianunezr <victorianunezr96@gmail.com>
2022-01-02 00:20:28 +0100

    generate plot for architecture comparison experiment

---
+++ jobs/architecture_comparison/cool_plots.py
@@ -0,0 +1,41 @@
+import os
+import pandas as pd
+import seaborn as sns
+
+from pathlib import Path
+import numpy as np
+import matplotlib.pyplot as plt
+
+def get_plot():
+
+    def game_name(csv:str):
+        name = str(csv)
+        return name.split('/')[-1].split('_')[1]
+
+    csv = list(Path(os.getcwd()).glob(f"jobs/all_results/impala/*.csv"))
+    plt.gcf().subplots_adjust(bottom=0.15)
+
+    fig, axes = plt.subplots(1, 4, sharex=True, figsize=(20,4))
+    rolling_window = 20
+    
+    for i in range(len(csv)):
+
+        df_result = pd.read_csv(csv[i]).assign(model='NatureDQN')
+        df_result.loc[:, 'rolling_mean_train'] = df_result.groupby('model')['normalized_reward_train'].rolling(rolling_window).mean().values
+        df_result.loc[:, 'rolling_mean_test'] = df_result.groupby('model')['normalized_reward_test'].rolling(rolling_window).mean().values
+
+        sns.lineplot(ax=axes[i], data=df_result, x='step', y='rolling_mean_train', label='train', legend=0)
+        sns.lineplot(ax=axes[i], data=df_result, x='step', y='rolling_mean_test', label='test', legend=0)
+        axes[i].set_title(game_name(csv[i]))
+        axes[i].set_ylabel('')    
+        axes[i].set_xlabel('')
+
+    handles, labels = axes[3].get_legend_handles_labels()
+    fig.legend(handles, labels, loc='upper right') 
+    fig.supxlabel('step')
+    fig.supylabel('normalized reward')
+
+    plt.show()
+    #plt.savefig(f'nature.png')
+
+get_plot()
